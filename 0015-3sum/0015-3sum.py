b874a174ebe8ab3741d16738784117a93423566c
K.yahoshuva <yahoshuva138@gmail.com>
2026-03-23 11:03:08 +0530

    Time: 747 ms (66.81%), Space: 18.4 MB (65.87%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -13,7 +13,7 @@ class Solution(object):
             
             l, r = i+1, len(nums)-1
 
-            while 1 < r:
+            while l < r:
                 s = nums[i] + nums[l] + nums[r]
                
                 if s == 0:
@@ -22,12 +22,12 @@ class Solution(object):
                     r -= 1 
 
                     while l < r and nums[l] == nums[l-1]:
-                    l += 1
-                    while l < r and numd[r] == nums[r+1]:
-                    r -= 1
+                        l += 1
+                    while l < r and nums[r] == nums[r+1]:
+                        r -= 1
 
                 elif s < 0:
-                    l += 2
+                    l += 1
                 else:
                     r -= 1
 
